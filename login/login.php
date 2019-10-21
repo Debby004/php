074f0ba456815ab25403fdeb9b997b20092cb5b6
Debby004 <debby_xia@qq.com>
2019-10-21 09:02:28 +0800

    提交原生php登录

--- login/login.php
+++ login/login.php
@@ -1,7 +1,49 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2019/10/20
- * Time: 16:31
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>登录页</title>
+</head>
+<body>
+    <form action="" method="get">
+        <p>
+          用户名：  <input type="text" class="username">
+        </p>
+        <p>
+          密码  <input type="password" class="password" id="password">
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+    </form>
+    <table>
+    <?php
+
+    $db = new  MySQLi("localhost","root","root","bysj");
+    $sql = "select * from user";
+    $result = $db->query($sql);
+    if($result){
+        while($attr = $result->fetch_row()){
+
+            echo "
+            <tr>
+                <td>{$attr[0]}</td>
+                <td>{$attr[1]}</td>
+                <td>{$attr[2]}</td>
+                <td>{$attr[3]}</td>
+                <td>{$attr[4]}</td>
+                <td>{$attr[5]}</td> 
+            </tr>            
+            ";
+
+        }
+    }
+
+    ?>
+    
+        
+    </table>
+</body>
+</html>
